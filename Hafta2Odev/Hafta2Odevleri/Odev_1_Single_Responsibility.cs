b72a40960ba22dac26ef5ada063d06ed0ed58f11
İsmail <ismailll.ozcelik@gmail.com>
2024-06-19 14:10:44 +0300

    Single responsibility completed

---
+++ Hafta2Odev/Hafta2Odevleri/Odev_1_Single_Responsibility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta2Odevleri
+{
+    public class Invoice
+    {
+        public void GenerateInvoice()
+        {
+            // Fatura oluşturma işlemleri
+            Console.WriteLine("Invoice generated.");
+        }
+    }
+
+    public class EmailSender
+    {
+        public void SendEmail(string email, string content)
+        {
+            // Email gönderme işlemleri
+            Console.WriteLine($"Email sent to {email} with content: {content}");
+        }
+    }
+
+    public class InvoiceService(Invoice invoice, EmailSender emailSender)
+    {
+
+        public void ProcessInvoice(string email)
+        {
+            invoice.GenerateInvoice();
+            emailSender.SendEmail(email, "Invoice has been generated.");
+        }
+    }
+}
---
+++ Hafta2Odev/Hafta2Odevleri/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// 1. Soru
+// Single Responsibility Principle (SRP) tek bir sınıfın sadece bir işi yapmasını önerir.
+using Hafta2Odevleri;
+
+InvoiceService invoiceService = new InvoiceService(new Invoice(), new EmailSender());
+invoiceService.ProcessInvoice("Send by SRP");
